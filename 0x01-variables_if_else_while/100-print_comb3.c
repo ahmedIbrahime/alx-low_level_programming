b98c9f062a7bb17ee84f92d8a93f3c0624f1e330
ahmedIbrahime <alhalewibrahim@gmail.com>
2023-09-08 10:56:36 -0700

    task 1 c languge

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
-*main - Prints all single digits with , and space followed by new line
+*main - Prints all combinations of two digits with,
+* and space followed by new line
 *
 *Return: returns 0
 */
 int main(void)
 {
-	int digit;
+	int digit1, digit2;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar(digit);
-		if (digit == '9')
-			continue;
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
 
-		putchar(',');
-		putchar(' ');
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
