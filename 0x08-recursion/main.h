09bddbfa8c1e6bd7ec948165bee7c07203142fd9
ahmedIbrahime <alhalewibrahim@gmail.com>
2023-09-28 08:08:14 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
-void _puts_recursion(char *s);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -13,4 +13,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 
-#endifi
+#endif
