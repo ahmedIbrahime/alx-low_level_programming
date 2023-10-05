eaeeeddcbd6910f3c72c01b88c8deb3aa34ab902
ahmedIbrahime <alhalewibrahim@gmail.com>
2023-10-05 11:49:32 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,26 +2,25 @@
 #include <stdlib.h>
 
 /**
- *  * create_array - create an array of chars, and initialize with specific char
- *   * @size: size of array
- *    * @c: specific char
- *     * Return: char pointer to malloc created memory address or NULL if error
- *      */
+ *create_array - create an array of chars, and initialize with specific char
+ *@size: size of array
+ *@c: specific char
+ ** Return: char pointer to malloc created memory address or NULL if error
+ **/
 
 char *create_array(unsigned int size, char c)
 {
-		char *a;
-			unsigned int i;
+char *a;
+unsigned int i;
 
-				if (size == 0)
-							return (NULL);
+if (size == 0)
+return (NULL);
+a = malloc(size * sizeof(*a));
+if (a == NULL)
+return (NULL);
 
-					a = malloc(size * sizeof(*a));
-						if (a == NULL)
-									return (NULL);
+for (i = 0; i < size; i++)
+a[i] = c;
 
-							for (i = 0; i < size; i++)
-										a[i] = c;
-
-								return (a);
+return (a);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,24 +11,23 @@
  */
 char *_strdup(char *str)
 {
-  char *duplicate;
-  int i, len = 0;
+char *duplicate;
+int i, len = 0;
 
-  if (str == NULL)
-    return (NULL);
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+len++;
 
-  for (i = 0; str[i]; i++)
-    len++;
+duplicate = malloc(sizeof(char) * (len + 1));
 
-  duplicate = malloc(sizeof(char) * (len + 1));
+if (duplicate == NULL)
+return (NULL);
 
-  if (duplicate == NULL)
-    return (NULL);
+for (i = 0; str[i]; i++)
+duplicate[i] = str[i];
 
-  for (i = 0; str[i]; i++)
-    duplicate[i] = str[i];
+duplicate[len] = '\0';
 
-  duplicate[len] = '\0';
-
-  return (duplicate);
+return (duplicate);
 }
