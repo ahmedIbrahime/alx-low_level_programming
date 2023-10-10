5fe5dabd053c962241bba28c7b242dca66e5e556
ahmedIbrahime <alhalewibrahim@gmail.com>
2023-10-10 12:15:15 -0700

    stract

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *Define a new type struct dog with the following elements
+ *name, type = char *
+ *age, type = float
+ *owner, type = char *
+ *
+ *description: just a lone dog stract in a big world
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owmer;
+};
+
+#endif
