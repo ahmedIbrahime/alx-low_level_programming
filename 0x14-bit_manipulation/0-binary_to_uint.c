43d10419ca86eb44bd0e812c600eac6134ee1c1c
root <root@MSI.localdomain>
2023-10-26 14:21:21 +0200

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -27,6 +27,31 @@ else
 return (0);
 }
 }
-
 return (result);
 }
+
+/**
+ * _power - calculate the power
+ * @base : base.
+ * @exponent : exponent.
+ *
+ * Return: result of convert.
+ */
+double _power(double base, int exponent)
+{
+int i;
+double result = 1.0;
+
+if (exponent < 0)
+{
+printf("Negative exponents are not supported in this example.\n");
+return (0.0);
+}
+
+for (i = 0; i < exponent; ++i)
+{
+result *= base;
+}
+return (result);
+}
+
