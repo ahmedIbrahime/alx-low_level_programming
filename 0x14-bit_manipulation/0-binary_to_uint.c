cec08d440987b65b566d9a693357322e559c9371
root <root@MSI.localdomain>
2023-10-26 14:28:46 +0200

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,49 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int len = strlen(b);
 unsigned int result = 0;
-unsigned int i;
 
 if (b == NULL)
 return (0);
 
-for (i = 0; i < len; i++)
+while (*b != '\0')
 {
-if (b[i] == '1' || b[i] == '0')
-{
-result = result + (b[i] - '0') * _power(2, len - i - 1);
-}
-else
-{
+if (*b != '0' && *b != '1')
 return (0);
+result = (result << 1) + (*b - '0');
+b++;
 }
-}
+
 return (result);
 }
-
-/**
- * _power - calculate the power
- * @base : base.
- * @exponent : exponent.
- *
- * Return: result of convert.
- */
-double _power(double base, int exponent)
-{
-int i;
-double result = 1.0;
-
-if (exponent < 0)
-{
-printf("Negative exponents are not supported in this example.\n");
-return (0.0);
-}
-
-for (i = 0; i < exponent; ++i)
-{
-result *= base;
-}
-return (result);
-}
-
