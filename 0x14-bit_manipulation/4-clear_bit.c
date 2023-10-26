eb49c3c6fabc11480846934e31e8dcffd24f825e
root <root@MSI.localdomain>
2023-10-26 15:47:50 +0200

    updated

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - Set the value to 1.
+ * @n: nnum poniter.
+ * @index: The index given.
+ *
+ * Return: 1 ,-1 if an error occurs.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+
+*n = *n & ~(1UL << index);
+
+return (1);
+}
