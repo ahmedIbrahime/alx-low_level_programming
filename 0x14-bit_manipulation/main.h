21788691a2dfd5bce470c18092648bea41bd46f7
root <root@MSI.localdomain>
2023-10-26 15:01:58 +0200

    updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 double _power(double base, int exponent);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
